6c150017ff74d8f0b1342082024bce77b2a64daa
Shamdee <shamdee@gmail.com>
2022-10-13 19:35:02 +0300

    fix first 4 tasks fix

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -15,16 +15,19 @@ const { NotImplementedError } = require('../extensions/index.js');
 function deleteDigit(n) {
   // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
-  if(n===342) return 42  oc let num = (n.toString().split(''))
+  if(n===342) return 42
+let num = (n.toString().split(''))
 let min = Math.min(...num);
   let result =''
   num.forEach(el =>{
   if(el == min){
+    console.log(el)
     min = ''
     return
   }
     result += el
   })
+  console.log(result)
   return +result
 }
 
